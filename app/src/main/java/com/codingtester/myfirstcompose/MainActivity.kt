4b51c488e191ba1c948d595d42dff8aaf17c5b62
Mohamed-Hosam-eldien <mohamedhosameldien07@gmail.com>
2022-11-24 02:48:10 +0200

    set size and change color to text

--- app/src/main/java/com/codingtester/myfirstcompose/MainActivity.kt
+++ app/src/main/java/com/codingtester/myfirstcompose/MainActivity.kt
@@ -3,21 +3,17 @@ package com.codingtester.myfirstcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import com.codingtester.myfirstcompose.ui.theme.MyFirstComposeTheme
+import androidx.compose.ui.unit.sp
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PrintText(name = "mohamed")
+            PrintText(name = "first compose")
         }
     }
 }
@@ -26,19 +22,7 @@ class MainActivity : ComponentActivity() {
 fun PrintText(name: String) {
     Text(
         text = name,
-        color = Color.Blue,
+        color = Color.Red,
+        fontSize = 24.sp
     )
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MyFirstComposeTheme {
-        Greeting("Android")
-    }
 }
